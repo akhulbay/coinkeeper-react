e59cd338e078e9fa9ce2fcff59cc5a5b7a36db04
T1m4ay <tero4y@gmail.com>
2023-04-01 10:54:27 +0600

    modified:   coinkeeper/src/App.js 	modified:   coinkeeper/src/components/Icons/building.svg 	modified:   coinkeeper/src/components/Main/Features/Features.js 	modified:   coinkeeper/src/components/Main/Features/Features.module.css 	modified:   coinkeeper/src/components/Main/Main.js

--- coinkeeper/src/App.js
+++ coinkeeper/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import { useEffect, useState } from "react";
+import storage from "./components/Icons/component";
+import icod from "./components/Icons/building.svg";
 
 function App() {
   const [income, setIncome] = useState(
@@ -9,17 +11,32 @@ function App() {
       {
         id: 1,
         title: "IITU",
-        icon: "",
+        sum: 100000,
       },
       {
         id: 2,
         title: "Salary",
-        // icon: ,
+        sum: 150000,
       },
       {
         id: 3,
         title: "Other",
-        // icon: ,
+        sum: 30000,
+      },
+      {
+        id: 4,
+        title: "Other",
+        sum: 0,
+      },
+      {
+        id: 5,
+        title: "Other",
+        sum: 0,
+      },
+      {
+        id: 6,
+        title: "Other",
+        sum: 0,
       },
     ]
   );
@@ -28,10 +45,93 @@ function App() {
     localStorage.setItem("income", JSON.stringify(income));
   }, [income]);
 
+  const [accounts, setAccounts] = useState(
+    JSON.parse(localStorage.getItem("accounts")) || [
+      {
+        id: 1,
+        title: "Kaspi",
+        sum: 25000,
+      },
+      {
+        id: 2,
+        title: "Halyk",
+        sum: 100000,
+      },
+      {
+        id: 3,
+        title: "Cash",
+        sum: 180000,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+  }, [accounts]);
+
+  const [expenses, setExpenses] = useState(
+    JSON.parse(localStorage.getItem("expenses")) || [
+      {
+        id: 1,
+        title: "Products",
+        maxSum: 30000,
+        totalSum: 15000,
+      },
+      {
+        id: 2,
+        title: "Clothing",
+        maxSum: 70000,
+        totalSum: 45000,
+        icon: icod,
+      },
+      {
+        id: 3,
+        title: "Home",
+        maxSum: 30000,
+        totalSum: 30000,
+      },
+      {
+        id: 4,
+        title: "Phone",
+        maxSum: 10000,
+        totalSum: 0,
+      },
+      {
+        id: 5,
+        title: "Transport",
+        maxSum: 25000,
+        totalSum: 1500,
+      },
+      {
+        id: 6,
+        title: "Entertain",
+        maxSum: 40000,
+        totalSum: 47000,
+      },
+      {
+        id: 7,
+        title: "Services",
+        maxSum: 15000,
+        totalSum: 12000,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   return (
     <div>
       <Header />
-      <Main income={income} setIncome={setIncome} />
+      <Main
+        income={income}
+        setIncome={setIncome}
+        accounts={accounts}
+        setAccounts={setAccounts}
+        expenses={expenses}
+        setExpense={setExpenses}
+      />
     </div>
   );
 }
--- coinkeeper/src/components/Main/Main.js
+++ coinkeeper/src/components/Main/Main.js
@@ -2,10 +2,24 @@ import ms from "./Main.module.css";
 import Features from "./Features/Features";
 import CostHistory from "./CostHistory/CostHistory";
 
-function Main({ income, setIncome }) {
+function Main({
+  income,
+  setIncome,
+  accounts,
+  setAccounts,
+  expenses,
+  setExpenses,
+}) {
   return (
     <div className={ms.main}>
-      <Features income={income} setIncome={setIncome} />
+      <Features
+        income={income}
+        setIncome={setIncome}
+        accounts={accounts}
+        setAccounts={setAccounts}
+        expenses={expenses}
+        setExpense={setExpenses}
+      />
       <CostHistory />
     </div>
   );
